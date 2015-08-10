c60fb0a2f0e6dd4a51c48202109b125f1c6eb625
Rémy-Christophe Schermesser <git@schermesser.com>
2015-08-11 00:57:07 +0200

    Refactor a bit, tine to :sleep:

--- src/parse.js
+++ src/parse.js
@@ -7,62 +7,34 @@ var parse_size = function(size) {
 	return parseInt(size, 10);
 };
 
+//date is already valid
 var parse_date = function(date) {
-	if(REGEXP.YEAR.test(date)) {
-		return {
-			year: parseInt(REGEXP.YEAR.exec(date)[1], 10)
-		};
+	var regexp_exec = REGEXP.FULL_TIMESTAMP.exec(date);
+	var result = {
+		year: parseInt(regexp_exec[1], 10) //date is valid, so at least a year
 	}
 
-	if(REGEXP.YEAR_AND_MONTH.test(date)) {
-		var result = REGEXP.YEAR_AND_MONTH.exec(date);
-		return {
-			year: parseInt(result[1], 10),
-			month: parseInt(result[2], 10)
-		}
+	if(regexp_exec[2]) {
+		result.month = parseInt(regexp_exec[2], 10);
 	}
 
-	if(REGEXP.FULL_DATE.test(date)) {
-		var result = REGEXP.FULL_DATE.exec(date);
-		return {
-			year: parseInt(result[1], 10),
-			month: parseInt(result[2], 10),
-			day: parseInt(result[3], 10)
-		}
+	if(regexp_exec[3]) {
+		result.day = parseInt(regexp_exec[3], 10);
 	}
 
-	if(REGEXP.YEAR_MONTH_DAY_AND_HOUR.test(date)) {
-		var result = REGEXP.YEAR_MONTH_DAY_AND_HOUR.exec(date);
-		return {
-			year: parseInt(result[1], 10),
-			month: parseInt(result[2], 10),
-			day: parseInt(result[3], 10),
-			hour: parseInt(result[4], 10)
-		}
+	if(regexp_exec[4]) {
+		result.hour = parseInt(regexp_exec[4], 10);
 	}
 
-	if(REGEXP.YEAR_MONTH_DAY_HOUR_AND_MINUTE.test(date)) {
-		var result = REGEXP.YEAR_MONTH_DAY_HOUR_AND_MINUTE.exec(date);
-		return {
-			year: parseInt(result[1], 10),
-			month: parseInt(result[2], 10),
-			day: parseInt(result[3], 10),
-			hour: parseInt(result[4], 10),
-			minute: parseInt(result[5], 10)
-		}
+	if(regexp_exec[5]) {
+		result.minute = parseInt(regexp_exec[5], 10);
 	}
 
-	if(REGEXP.FULL_TIMESTAMP.test(date)) {
-		var result = REGEXP.FULL_TIMESTAMP.exec(date);
-		return {
-			year: parseInt(result[1], 10),
-			month: parseInt(result[2], 10),
-			day: parseInt(result[3], 10),
-			hour: parseInt(result[4], 10),
-			minute: parseInt(result[5], 10),
-			second: parseInt(result[6], 10)
-		}
+	if(regexp_exec[6]) {
+		result.second = parseInt(regexp_exec[6], 10);
 	}
+
+	return result;
 };
 
 module.exports = {
--- src/validator.js
+++ src/validator.js
@@ -1,40 +1,11 @@
 'use strict';
 
 var REGEXP = {
-	YEAR: /^(\d+)$/,
-	YEAR_AND_MONTH: /^(\d+)-(\d\d?)$/,
-	FULL_DATE: /^(\d+)-(\d\d?)-(\d\d?)$/,
-	YEAR_MONTH_DAY_AND_HOUR: /^(\d+)-(\d\d?)-(\d\d?) (\d\d?)$/,
-	YEAR_MONTH_DAY_HOUR_AND_MINUTE: /^(\d+)-(\d\d?)-(\d\d?) (\d\d?):(\d\d?)$/,
-	FULL_TIMESTAMP: /^(\d+)-(\d\d?)-(\d\d?) (\d\d?):(\d\d):(\d\d)$/
+	FULL_TIMESTAMP: /^(\d+)-?(\d\d?)?-?(\d\d?)? ?(\d\d?)?:?(\d\d)?:?(\d\d)?$/
 }
 
 var validate_date = function(date) {
-	if(REGEXP.YEAR.test(date)) {
-		return true;
-	}
-
-	if(REGEXP.YEAR_AND_MONTH.test(date)) {
-		return true;
-	}
-
-	if(REGEXP.FULL_DATE.test(date)) {
-		return true;
-	}
-
-	if(REGEXP.YEAR_MONTH_DAY_AND_HOUR.test(date)) {
-		return true;
-	}
-
-	if(REGEXP.YEAR_MONTH_DAY_HOUR_AND_MINUTE.test(date)) {
-		return true;
-	}
-
-	if(REGEXP.FULL_TIMESTAMP.test(date)) {
-		return true;
-	}
-
-	return false;
+	return REGEXP.FULL_TIMESTAMP.test(date);
 };
 
 var validate_size = function(size) {
